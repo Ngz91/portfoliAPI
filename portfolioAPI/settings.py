3042f9417ac4a7f627e6c9913c8a80c0a6a51586
Nevio Gomez <neviogomez91@gmail.com>
2022-06-22 16:17:39 -0400

    Update settings.py
    Ready to deploy

--- portfolioAPI/settings.py
+++ portfolioAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(nu1#nx$o+evvz4i8h)us7=2a^y673ab%b^+4nw=yxh@48@o!%'
+SECRET_KEY = os.environ.get('SECRET_KEY', True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolioAPI.urls'
