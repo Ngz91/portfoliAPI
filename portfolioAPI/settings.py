3b3e9558475024fff017888e2734cce3c46d5ff2
Nevio Gomez <neviogomez91@gmail.com>
2023-01-04 15:33:34 -0400

    Modify Allowed hosts

--- portfolioAPI/settings.py
+++ portfolioAPI/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ngzportfolioapi.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["ngzportfolioapi.herokuapp.com", "127.0.0.1", "web-production-f691.up.railway.app"]
 
 
 # Application definition
